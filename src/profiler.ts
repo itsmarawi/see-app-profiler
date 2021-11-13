d93855812035356dc63e99c4714b3fa5a6c48bfe
fencim <fencim@gmail.com>
2021-11-13 12:42:14 +0800

    encode info to base64

--- src/profiler.ts
+++ src/profiler.ts
@@ -308,7 +308,7 @@ export abstract class Profiler<P extends EveryProfile> {
                 NaclUtil.decodeBase64(encrypted),
                 pubKey);
             if (!decoded) throw new ProfilerException('permission-denied', 403);
-            return decoded;
+            return NaclUtil.encodeBase64(decoded);
         } catch (error) {
             throw new ProfilerException('invalid-activation', 403);
         }
